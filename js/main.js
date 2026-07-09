ff92ad03fb32ebd400da8c19e5a47e05d32cbd88
thebarmybrit-svg <thebarmybrit@gmail.com>
2026-07-09 14:04:19 +0100

    Implemented toggle button functionality for sidebar conntent

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
+// Navigation bar
+
+$('.btn--toggle').on('click', function(){
+    $('#sidebar-section').toggleClass('is-open');
+});
+$(window).on('resize', function() {
+    if (window.innerWidth >= 576) {
+        $('#sidebar-section').removeClass('is-open');
+    }
+});
+
+// Header
 var typed = new Typed(".typewriter", {
     strings: ["Designer", "Developer", "Wizard"],
     typeSpeed: 150,
     backSpeed: 50,
     typeDelay: 1500,
     loop: true
-
 });
