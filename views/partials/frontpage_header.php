27369c57cb3fc4e3635b13526582928ce67d1592
thebarmybrit-svg <thebarmybrit@gmail.com>
2026-09-04 11:23:33 +0100

    adjusted headers to allow for google site verification

--- views/partials/frontpage_header.php
+++ views/partials/frontpage_header.php
@@ -4,8 +4,8 @@
         <title>Alexander&apos;s Portfolio</title>
         <meta http-equiv="X-UA-Compatable" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="ROBOTS" content="NOINDEX,NOFOLLOW">
         <meta name="description" content="<?= htmlspecialchars($description) ?>">
+        <meta name="google-site-verification" content="naB_HLGsuZvx_bnel_e_vargKwWj3aUkYZGgyHGVAgQ">
         <link rel="stylesheet" href="js/pageCrossfade/pagecrossfade.css">
         <link rel="stylesheet" href="js/slick/slick.css">
         <link rel="stylesheet" href="css/styles.css">
